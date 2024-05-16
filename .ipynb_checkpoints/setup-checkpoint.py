e473386fd107a46d10e5c91368d4bce1f40171ea
masauer2 <masauer2@asu.edu>
2024-05-16 09:37:36 -0700

    add example

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='MASAnalysis',
-    version='0.1.0',
-    packages=find_packages(include=['MASAnalysis', 'MASAnalysis.*']),
-    install_requires=['numpy>=1.14.5']
-)
